e79c5fcebe1ee0c8d5ca01b772cd502617f6d159
lbui2602 <buiducluong2602@gmail.com>
2023-08-31 15:26:29 +0700

    mn

---
+++ app_demogit1/app/src/main/java/com/example/app_demogit1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.app_demogit1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "fdf", Toast.LENGTH_SHORT).show();
+    }
+}
